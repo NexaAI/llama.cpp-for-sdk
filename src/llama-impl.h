5b5b6b455e4b5e0b5ed52a93360c3440bfd8867c
Paul Zhu <paulzhuzx@gmail.com>
2025-07-20 19:34:08 -0700

    Fix windows build

--- src/llama-impl.h
+++ src/llama-impl.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ggml.h" // for ggml_log_level
+#include "llama.h" // for LLAMA_API
 
 #include <string>
 #include <vector>
@@ -20,8 +21,8 @@
 //
 
 LLAMA_ATTRIBUTE_FORMAT(2, 3)
-void llama_log_internal        (ggml_log_level level, const char * format, ...);
-void llama_log_callback_default(ggml_log_level level, const char * text, void * user_data);
+LLAMA_API void llama_log_internal        (ggml_log_level level, const char * format, ...);
+LLAMA_API void llama_log_callback_default(ggml_log_level level, const char * text, void * user_data);
 
 #define LLAMA_LOG(...)       llama_log_internal(GGML_LOG_LEVEL_NONE , __VA_ARGS__)
 #define LLAMA_LOG_INFO(...)  llama_log_internal(GGML_LOG_LEVEL_INFO , __VA_ARGS__)
